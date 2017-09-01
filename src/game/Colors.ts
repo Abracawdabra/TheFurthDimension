efb7f86e8463a3cf13403f7247c269e2ee15069f
Cawdabra <cawdabra@null.net>
2017-09-01 09:26:40 -0300

    Added Colors module and a clean command to package.json.

---
+++ src/game/Colors.ts
@@ -0,0 +1,10 @@
+/**
+ * The Furth Dimension
+ * @author Cawdabra <cawdabra@null.net>
+ * @license MIT
+ */
+
+export const GB_COLOR_DARKEST_GREEN  = "#0f380f";
+export const eGB_COLOR_DARK_GREEN     = "#306230";
+export const GB_COLOR_LIGHT_GREEN    = "#8bac0f";
+export const GB_COLOR_LIGHTEST_GREEN = "#9bbc0f";
