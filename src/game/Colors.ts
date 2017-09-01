6302dd8e7186d670daf47acfb716bc3a472a8f46
Cawdabra <cawdabra@null.net>
2017-09-01 09:41:25 -0300

    Fixed typo in Colors module.

--- src/game/Colors.ts
+++ src/game/Colors.ts
@@ -5,6 +5,6 @@
  */
 
 export const GB_COLOR_DARKEST_GREEN  = "#0f380f";
-export const eGB_COLOR_DARK_GREEN     = "#306230";
+export const GB_COLOR_DARK_GREEN     = "#306230";
 export const GB_COLOR_LIGHT_GREEN    = "#8bac0f";
 export const GB_COLOR_LIGHTEST_GREEN = "#9bbc0f";
